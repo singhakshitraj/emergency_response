96171adc4d390e2eb9d230b33547dda2ab575cf2
Akshit Raj Singh <akshitraj.rkt@gmail.com>
2025-06-24 16:16:51 +0530

    cors added

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI,status
 from routers import contacts_emergency,additional_info,emergency
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(contacts_emergency.router)
 app.include_router(additional_info.router)
 app.include_router(emergency.router)
